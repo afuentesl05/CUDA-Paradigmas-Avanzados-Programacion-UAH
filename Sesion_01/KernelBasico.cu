4ffcedabfcf4da10551ed9ce7018bb829301892c
Álvaro Fuentes Lozano <167740606+afuentesl05@users.noreply.github.com>
2026-02-06 20:19:10 +0100

    Add basic CUDA kernel implementation with main function

---
+++ Sesion_01/KernelBasico.cu
@@ -0,0 +1,18 @@
+﻿#include <cstdio>
+#include <cuda_runtime.h>
+
+__global__ void basicKernel() {
+    int blq = blockIdx.x;
+    int dim = blockDim.x;
+    int tId = threadIdx.x;
+    int index = blq * dim + tId;
+
+    printf("Thread #%d. Bloque #%d, hilo #%d\n", index, blq, tId);
+}
+
+int main() {
+    basicKernel << <3, 4 >> > ();
+    cudaDeviceSynchronize();
+    return 0;
+}
+
